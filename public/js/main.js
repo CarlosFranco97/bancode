e041a267536615788a42d04a10642932d5a300ab
CarlosFranco97 <carlosfranco63.cf@gmail.com>
2024-10-02 22:15:22 -0500

    Se agrega el menu hamburguesa a la seccion preguntas.html y a la seccion index.html

---
+++ public/js/main.js
@@ -0,0 +1,38 @@
+
+
+const hamburguerMenu = ({target}) => {
+
+   const activeHamburguerMenu = target.classList.toggle('menu');
+
+
+   const classNav = document.getElementsByClassName('contenedor-nav-botones-header');
+
+   const header = document.querySelector('header');
+
+
+    if(activeHamburguerMenu) {
+        classNav[0].classList.add('contenedor-nav-botones-header__oculta');
+        header.classList.add('header__menu-abierto');
+    } else {      
+        classNav[0].classList.remove('contenedor-nav-botones-header__oculta');
+        header.classList.remove('header__menu-abierto');
+    }           
+    
+}
+
+const containerNavResize = () => {
+    const classNav = document.getElementsByClassName('contenedor-nav-botones-header');
+    
+    if(window.innerWidth >= 768) {
+
+        classNav[0].classList.remove('contenedor-nav-botones-header__oculta');
+
+    } 
+
+}
+
+
+window.addEventListener('resize', containerNavResize);
+
+window.addEventListener('load', containerNavResize);
+
